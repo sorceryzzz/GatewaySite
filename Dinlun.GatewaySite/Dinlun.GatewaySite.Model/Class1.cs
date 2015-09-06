ffd5b7fffce17d5b03a47342e335f76f341c169b
sorceryzzz <sorcery.zzz@gmail.com>
2015-09-06 13:35:33 +0800

    add project--------build ok

---
+++ Dinlun.GatewaySite/Dinlun.GatewaySite.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinlun.GatewaySite.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ Dinlun.GatewaySite/Dinlun.GatewaySite.WebPoint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dinlun.GatewaySite.WebPoint.Startup))]
+namespace Dinlun.GatewaySite.WebPoint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
