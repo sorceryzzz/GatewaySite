a44adaae04c66260906046908b3800c3611b4011
sorceryzzz <sorcery.zzz@gmail.com>
2015-09-15 08:45:07 +0800

    Create NewsMsg to model project-----bulid ok

---
+++ Dinlun.GatewaySite/Dinlun.GatewaySite.Model/News/NewsMsg.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinlun.GatewaySite.Model.News
+{
+   public  class NewsMsg
+   {
+       #region - feild -
+       private int _ID;
+       private string _title;
+       private string _content;
+       private int _newsType;
+       private string _imageUrl;
+       private DateTime _insertTime;
+       private string _avg1;
+       private string _avg2;
+       #endregion
+
+       #region - property -
+       /// <summary>
+       /// 主键
+       /// </summary>
+       public int ID { set { _ID = value; } get { return _ID; } }
+       /// <summary>
+       /// 新闻标题
+       /// </summary>
+       public string Title { set { _title = value; } get { return _title; } }
+       /// <summary>
+       /// 新闻内容
+       /// </summary>
+       public string Content { set { _content = value; } get { return _content; } }
+       /// <summary>
+       /// 新闻类别
+       /// </summary>
+       public int NewsType { set { _newsType = value; } get { return _newsType; } }
+       /// <summary>
+       /// 图片链接
+       /// </summary>
+       public string ImageUrl { set { _imageUrl = value; } get { return _imageUrl; } }
+       /// <summary>
+       ///记录生成时间
+       /// </summary>
+       public DateTime InsertTime { set { _insertTime = value; } get { return _insertTime; } }
+       /// <summary>
+       /// 扩展字段1
+       /// </summary>
+       public string Avg1 { set { _avg1 = value; } get { return _avg1; } }
+       /// <summary>
+       /// 扩展字段2
+       /// </summary>
+       public string Avg2 { set { _avg2 = value; } get { return _avg2; } }
+
+       #endregion
+   }
+}
---
+++ Document/DB/dinlun.sql
@@ -0,0 +1,34 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.6.25 : Database - dinlun
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `newsmsg` */
+
+DROP TABLE IF EXISTS `newsmsg`;
+
+CREATE TABLE `newsmsg` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `Title` varchar(255) NOT NULL COMMENT '新闻标题',
+  `Content` text NOT NULL COMMENT '新闻内容',
+  `NewsType` int(11) NOT NULL COMMENT '企业动态 1 行业新闻 2 媒体新闻 3',
+  `ImgUrl` varchar(255) DEFAULT NULL COMMENT '图片地址',
+  `InsertTime` datetime NOT NULL COMMENT '生成时间',
+  `avg1` varchar(5) DEFAULT NULL COMMENT '扩展字段1',
+  `avg2` varchar(5) DEFAULT NULL COMMENT '扩展字段2',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
