29ab62c29fa5b3dffc00682d4db2157ba354bfde
sorceryzzz <sorcery.zzz@gmail.com>
2015-09-24 08:37:41 +0800

    create model build ok

---
+++ Dinlun.GatewaySite/Dinlun.GatewaySite.Model/Contact/ContactDescModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinlun.GatewaySite.Model.Contact
+{
+   public class ContactDescModel
+   {
+
+       #region - feild -
+       private int _id;
+       private string _contactWay;
+       private string _place;
+       private DateTime _inserTime;
+       private DateTime _updateTime;
+       private string _avg1;
+       private string _avg2;
+       #endregion
+
+       #region - property -
+       /// <summary>
+       /// 主键
+       /// </summary>
+       public int ID { set { _id = value; } get{return _id;} }
+       /// <summary>
+       /// 联系方式
+       /// </summary>
+       public string ContactWay { set { _contactWay = value; } get { return _contactWay; } }
+       /// <summary>
+       /// 地理位置
+       /// </summary>
+       public string Place{set{_place=value;}get{return _place;}}
+       /// <summary>
+       /// 记录生成时间
+       /// </summary>
+       public DateTime InserTime { set { _inserTime = value; } get { return _inserTime; } }
+
+       /// <summary>
+       /// 更新时间
+       /// </summary>
+       public DateTime UpdateTime { set { _updateTime = value; } get { return _updateTime; } }
+
+       /// <summary>
+       /// 扩展字段1
+       /// </summary>
+       public string Avg1 { set { _avg1 = value; } get { return _avg1; } }
+
+       /// <summary>
+       /// 扩展字段2
+       /// </summary>
+       public string Avg2 { set { _avg2 = value; } get { return _avg2; } }
+
+       #endregion
+
+
+   }
+}
---
+++ Dinlun.GatewaySite/Dinlun.GatewaySite.Model/ZhaoPin/ZhaoPinModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinlun.GatewaySite.Model.ZhaoPin
+{
+   public  class ZhaoPinModel
+   {
+       #region - feild -
+       private int _id;
+       private string _dutyDesc;
+       private string _descDetail;
+       private string _place;
+       private DateTime _inserTime;
+       private DateTime _updateTime;
+       private string _avg1;
+       private string _avg2;
+       #endregion
+
+       #region - property -
+       /// <summary>
+       /// 主键
+       /// </summary>
+       public int ID { set { _id = value; } get { return _id; } }
+       /// <summary>
+       /// 岗位职责
+       /// </summary>
+       public string DutyDesc { set { _dutyDesc = value; } get { return _descDetail; } }
+       /// <summary>
+       /// 岗位描述
+       /// </summary>
+       public string DescDetail { set { _descDetail = value; } get { return _descDetail; } }
+       /// <summary>
+       /// 工作地点
+       /// </summary>
+       public string Place { set { _place = value; } get { return _place; } }
+       /// <summary>
+       /// 记录生成时间
+       /// </summary>
+       public DateTime InserTime { set { _inserTime = value; } get { return _inserTime; } }
+
+       /// <summary>
+       /// 更新时间
+       /// </summary>
+       public DateTime UpdateTime { set { _updateTime = value; } get { return _updateTime; } }
+
+       /// <summary>
+       /// 扩展字段1
+       /// </summary>
+       public string Avg1 { set { _avg1 = value; } get { return _avg1; } }
+
+       /// <summary>
+       /// 扩展字段2
+       /// </summary>
+       public string Avg2 { set { _avg2 = value; } get { return _avg2; } }
+       #endregion
+   }
+}
